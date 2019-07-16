c1a6daa1db5e1030d02f0a2174362145f73c53ab
saitama <snusmoomroomia2020@gmail.com>
2019-07-17 01:09:23 +0900

    next question function

--- src/components/Quiz.vue
+++ src/components/Quiz.vue
@@ -1,89 +1,87 @@
 <template>
-    <div class="quiz-container">
-      <h2>Lesson 1</h2>
-      <p>Lesson topic</p>
-      <div class="question" v-for="(q, index) in quiz" :key="index">
-        <div v-if="qIndex===index" v-html="q.question"></div>
-        <div class="" @click="next">
-          <div class="answer" v-for="a in q.answers">
-            <label v-if="qIndex===index" for="index">
-              <input type="radio" :name="index" :value="a.correct">
-                {{ a.literal }} {{ a.text }}
-            </label>
-          </div>
-        </div>
-      </div>
-    </div>
+	<div class="quiz-container-fluid">
+		<div class="container">
+			<h2>Lesson 1</h2>
+			<p>Lesson topic</p>
+			<div class="question" v-for="(q, index) in quiz" :key="index">
+				<div class="question-item" v-if="questionIndex===index" v-html="q.question"></div>
+				<div @click.prevent="next" v-if="isCorrect">
+					<div class="answer" v-for="a in q.answers" :key="a.text">
+						<label v-if="questionIndex===index">
+							<input type="radio" :name="index" :value="a.correct" />({{ a.literal }}) {{ a.text }}						
+						</label>
+					</div>
+				</div>
+			</div>
+			<div v-show="!isCorrect">
+				<div class="wrong">{{ picked }}</div>
+				<div class="correct">{{ correct }}</div>
+				<p>try again</p>
+			</div>
+		</div>
+	</div>
 </template>
 
 <script>
+import { quiz as quizList } from "../quizList"
 export default {
-  data() {
-    return {
-      qIndex: 0,
-      quiz: [
-          {
-            question:
-              `Which of the following statements concerning risk are correct?
-                <br>
-                i. All risks are insurable <br>
-                ii. Not all risk are commerically insurable <br>
-                iii. The only remedy for any kind of risk is insurance <br>
-                iv. Insurers may use the work risk with other meanings`,
-              answers: [
-                {
-                  text: "i and ii",
-                  literal: "a"
-                },
-                {
-                  text: "ii and iii",
-                  literal: "b"
-                },
-                {
-                  text: "ii and iv",
-                  literal: "c",
-                  correct: true
-                },
-                {
-                  text: "iii and iv",
-                  literal: "d"
-                }
-              ]
+	data() {
+		return {
+			questionIndex: 0,
+			isCorrect: true,
+			correct: '',
+			picked: null,	
+			quiz: quizList,
+		}
+	},
+	methods: {
+		next(e) {
+			if (e.target.value) { 
+				this.questionIndex++
+			}
+			else {
+				this.isCorrect = false
+			}  
 
-            },
-            {
-            question:
-                `Risk may be described as the uncertainty concerning a potential loss.
-                Which of the following potential losses are insurable?`,
-            answers: [
-              {
-                text: "iFinancial and Physical",
-                literal: "a",
-                correct: true
-              },
-              {
-                text: "Emotional and Physical",
-                literal: "b"
-              },
-              {
-                text: "Emotional and Financial",
-                literal: "c",
-              },
-              {
-                text: "inancial , Physical and Emotional",
-                literal: "d"
-              }
-          ]
-        }
-      ]
-    }
-  },
-  methods: {
-    next(e) {
-      console.log(e.target)
-      if(e.target.value == true)
-        this.qIndex++
-    }
-  }
+			this.picked = e.target.innerText
+
+			let correct = this.quiz[this.questionIndex].answers.filter(corr => corr.hasOwnProperty('correct'))
+			this.correct = `(${correct[0].literal}) ${correct[0].text}`
+		}
+	}
 }
 </script>
+
+<style lang="scss">
+html, body {
+	margin: 0;
+	overflow: hidden;
+}
+.quiz-container-fluid {
+	width: 100vw;
+	height: 100vh;
+	padding: 20px;	
+	background-color: #F5F6FC;
+
+	.container {
+		max-width: 800px;
+		margin: auto;
+		text-align: left;
+
+		.question-item {
+			max-width: 100%;
+			background-color: #fff;
+			box-sizing: border-box;
+			padding: 16px;
+		}
+	}
+}
+
+.wrong {
+	background-color: rgb(235, 43, 43);
+}
+.correct {
+	background-color: rgb(43, 235, 53);
+}
+</style>
+
---
+++ src/quizList.js
@@ -0,0 +1,52 @@
+export const quiz = [
+    {
+        question: `Which of the following statements concerning risk are correct?
+            <br>
+            i. All risks are insurable <br>
+            ii. Not all risk are commerically insurable <br>
+            iii. The only remedy for any kind of risk is insurance <br>
+            iv. Insurers may use the work risk with other meanings`,
+        answers: [
+            {
+                text: 'i and ii',
+                literal: 'a'
+            },
+            {
+                text: 'ii and iii',
+                literal: 'b'
+            },
+            {
+                text: 'ii and iv',
+                literal: 'c',
+                correct: true
+            },
+            {
+                text: 'iii and iv',
+                literal: 'd'
+            }
+        ]
+    },
+    {
+        question: `Risk may be described as the uncertainty concerning a potential loss.
+            Which of the following potential losses are insurable?`,
+        answers: [
+            {
+                text: 'iFinancial and Physical',
+                literal: 'a',
+                correct: true
+            },
+            {
+                text: 'Emotional and Physical',
+                literal: 'b'
+            },
+            {
+                text: 'Emotional and Financial',
+                literal: 'c'
+            },
+            {
+                text: 'inancial , Physical and Emotional',
+                literal: 'd'
+            }
+        ]
+    }
+]
